67318267acca0d97e7fb0f5ac0f5c5631dd70191
iAD <iadmail1987@gmail.com>
2023-04-16 20:35:34 +0300

    "Работает 2"

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
-def heapify(arr, n, i):
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
-
-    if l < n and arr[l] > arr[largest]:
-        largest = l
-
-    if r < n and arr[r] > arr[largest]:
-        largest = r
-
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
 
 
-def heapSort(arr):
-    n = len(arr)
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-    for i in range(n//2 - 1, -1, -1):
-        heapify(arr, n, i)
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            curr_node = self.head
+            while curr_node.next:
+                curr_node = curr_node.next
+            curr_node.next = new_node
+            new_node.prev = curr_node
 
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
+    def reverse(self):
+        curr_node = self.head
+        prev_node = None
+        while curr_node:
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            curr_node.prev = next_node
+            prev_node = curr_node
+            curr_node = next_node
+        self.head = prev_node
+
+    def print_list(self):
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data)
+            curr_node = curr_node.next
+
 
 
 if __name__ == '__main__':
-    arr = [12, 11, 13, 5, 6, 7]
-    heapSort(arr)
-    print("Отсортированный массив:", arr)
+    linked_list = LinkedList()
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+    linked_list.append(4)
+
+    print("До разворота:")
+    linked_list.print_list()
+
+    linked_list.reverse()
+
+    print("После разворота:")
+    linked_list.print_list()
+
